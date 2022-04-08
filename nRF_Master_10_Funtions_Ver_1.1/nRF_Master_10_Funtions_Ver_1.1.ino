ada419ed1bc746e7a5c1ce7cff0e3d77cf0b5ed8
Peter Rhoades <100319240+PeterRhoades@users.noreply.github.com>
2022-04-08 20:59:24 +0100

    Add files via upload

---
+++ nRF_Master_10_Funtions_Ver_1.1/nRF_Master_10_Funtions_Ver_1.1.ino
@@ -0,0 +1,143 @@
+/*Peter Rhoades©:
+Sunday 06/02/22:
+nRF Reciver 10 Funtions®                             
+06/02/22 Stop command 11 added.                               
+                               
+                                   nRF Tranciver 
+ msg[0] 1 = Left                  1 = GRN  Black   
+ msg[0] 2 = Right                 2 = 3.3v Red
+ msg[0] 3 = Forward50             3 = CE   Pin 7  Blue
+ msg[0] 4 = Forward100            4 = CSN  Pin 8  Breen
+ msg[0] 5 = Reverse50             5 = SCK  Pin 13 Yellow
+ msg[0] 6 = Reverse100            6 = MOSI Pin 11 White
+ msg[0] 7 = Button C              7 = MISO Pin 12 Orange
+ msg[0] 8 = Button Z               _______________________
+ msg[0] 9 = Nunchuk Foward         | 1 | 2 |              |
+ msg[0] 10 = Nunchuk Reverse       | 3 | 4 |              | 
+ msg[0] 11 = Stop                  | 5 | 6 |              |
+                                   |_7_|___|______________|
+*/                                 
+
+#include <Wire.h>
+#include  <SPI.h>
+#include "RF24.h"
+#include "nunchuk.h"
+byte msg[1];
+RF24 radio(7,8);
+const uint64_t pipe = 0x8675309ABCll; //Pipe Number same as nRF Riciver:
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("In Setup");
+  Wire.begin();
+  nunchuk_init();
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  Serial.println("Master Nunchuk Ready");
+
+}
+
+void loop() {
+ (nunchuk_read()); 
+ 
+ delay(50);
+ 
+ if (nunchuk_joystickX() < -120) //Joystick Left: 
+  {
+  int left = nunchuk_joystickX();
+  msg[0] = 1; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Left Turn "); Serial.print (left);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_joystickX() > 120) //Joystick Right: 
+  {
+  int right = nunchuk_joystickX();
+  msg[0] = 2; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Right Turn "); Serial.print (right);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_joystickY() > 20 && nunchuk_joystickY() < 80) //Joystick forward 50%:
+  {
+  int forward50 = nunchuk_joystickY();
+  msg[0] = 3; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Formard 50% "); Serial.print (forward50);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_joystickY() > 80) //Joystick forward 100%:
+  {
+  int forward100 = nunchuk_joystickY();
+  msg[0] = 4; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Formard 100% "); Serial.print (forward100);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_joystickY() < -20 && nunchuk_joystickY() > -80) //Joystick reverse50%:
+  {
+  int reverse50 = nunchuk_joystickY();
+  msg[0] = 5; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Reverse 50% "); Serial.print (reverse50);
+   Serial.println("  Sender");
+  }
+  
+ if (nunchuk_joystickY() < -80) //Joystick reverse100%:
+  {
+  int reverse100 = nunchuk_joystickY();
+  msg[0] = 6; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Reverse 100% "); Serial.print (reverse100);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_buttonC() > 0) //Top Button Clossed
+    {
+  int buttonc = nunchuk_buttonC();
+  msg[0] = 7; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = ButtonC "); Serial.print (buttonc);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_buttonZ() > 0) //Bottom Button Clossed
+  {
+  int buttonz = nunchuk_buttonZ();
+  msg[0] = 8; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = ButtonZ "); Serial.print (buttonz);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_accelY() > 150) //Nunchuk Foward
+  {
+      int nunchukF = nunchuk_accelY();
+  msg[0] = 9; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Nunchuk Foward "); Serial.print (nunchukF);
+   Serial.println("  Sender");
+  }
+ if (nunchuk_accelY() < -150) //Nunchuk Reverse
+  {
+      int nunchukR = nunchuk_accelY();
+  msg[0] = 10; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.print (" = Nunchuk Reverse "); Serial.print (nunchukR);
+   Serial.println("  Sender");
+ }
+ if (nunchuk_joystickX() > -120 && nunchuk_joystickX() < 120 && nunchuk_joystickY() < 20 && nunchuk_joystickY() > -20 && nunchuk_buttonZ() <1 &&nunchuk_buttonC()< 1)
+ {
+  msg[0] = 11; 
+  radio.write(msg, sizeof(msg));
+   Serial.print(msg[0]);
+   Serial.println (" = Stop Sender"); 
+}
+}
---
+++ nRF_Reciver_10_Funtions_Ver_1.2/nRF_Reciver_10_Funtions_Ver_1.2.ino
@@ -0,0 +1,161 @@
+/*Peter Rhoades©:
+Sunday 06/02/22:
+nRF Reciver 10 Funtions®
+06/02/22 Stop command 11 added:
+ 
+ nRF Tranciver 
+  1 = GRN  Black   
+  2 = 3.3v Red
+  3 = CE   Pin 7  Blue
+  4 = CSN  Pin 8  Breen
+  5 = SCK  Pin 13 Yellow
+  6 = MOSI Pin 11 White
+  7 = MISO Pin 12 Orange
+   _______________________
+  | 1 | 2 |              |
+  | 3 | 4 |              | 
+  | 5 | 6 |              |
+  |_7_|___|______________|
+*/
+
+#include  <SPI.h>
+#include "RF24.h"
+byte msg[1];
+RF24 radio(7,8);
+const uint64_t pipe = 0x8675309ABCll; //Pipe Number same as nRF Sender:
+
+// Motor A Right connections
+int enA = 9;
+int in1 = A0;
+int in2 = A1;
+// Motor B connections
+int enB = 3;
+int in3 = A2;
+int in4 = A3;
+int myTime = millis();
+
+void setup(void)
+{
+  
+ Serial.begin(9600);
+  Serial.println("In Setup");
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+ pinMode(enA, OUTPUT); // Set all the motor control pins to outputs:
+ pinMode(enB, OUTPUT);
+ pinMode(in1, OUTPUT);
+ pinMode(in2, OUTPUT);
+ pinMode(in3, OUTPUT);
+ pinMode(in4, OUTPUT);
+  digitalWrite(in1, LOW); // Turn off motors - Initial State:
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  Serial.println("Slave Nunchuk Ready");
+ }
+
+void loop(void)
+  {
+  if(radio.available())
+  {
+  
+    radio.read(msg, sizeof(msg));
+    //delay(50);
+    
+  if(msg[0] == 1)
+  {
+  Serial.print(" Left Turn ");
+  Serial.println("Reciver");
+  analogWrite(enA, 255); //Turn on motor A & B
+  analogWrite(enB, 255); 
+ digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, HIGH);
+  }
+  else if(msg[0] == 2)
+  {
+   Serial.print(" Right Turn ");
+   Serial.println("Reciver");
+   analogWrite(enA, 255); //Turn on motor A & B
+   analogWrite(enB, 255); 
+ digitalWrite(in1, LOW);
+ digitalWrite(in2, HIGH);
+ digitalWrite(in3, HIGH);
+ digitalWrite(in4, LOW);
+  }
+  else if(msg[0] == 3)
+  {
+   Serial.print(" Forward 50% ");
+   Serial.println("Reciver");
+   analogWrite(enA, 155); //Turn on motor A & B
+   analogWrite(enB, 155); 
+ digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, HIGH);
+ digitalWrite(in4, LOW);
+  }
+  else if(msg[0] == 4)
+  {
+   Serial.print(" Forward 100% ");
+   Serial.println("Reciver");
+   analogWrite(enA, 255); //Turn on motor A & B
+   analogWrite(enB, 255); 
+ digitalWrite(in1, HIGH);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, HIGH);
+ digitalWrite(in4, LOW);
+  }
+  else if(msg[0] == 5)
+  {
+   Serial.print(" Reverse 50% ");
+   Serial.println("Reciver");
+   analogWrite(enA, 155); //Turn on motor A & B
+   analogWrite(enB, 155); 
+ digitalWrite(in1, LOW);
+ digitalWrite(in2, HIGH);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, HIGH);
+  }
+  else if(msg[0] == 6)
+  {
+   Serial.print(" Reverse 100% ");
+   Serial.println("Reciver");
+   analogWrite(enA, 255); //Turn on motor A & B
+   analogWrite(enB, 255); 
+ digitalWrite(in1, LOW);
+ digitalWrite(in2, HIGH);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, HIGH);
+  }
+  else if(msg[0] == 7)
+  {
+   Serial.print(" Button C ");
+   Serial.println("Reciver");
+  }
+  else if(msg[0] == 8)
+  {
+   Serial.print(" Button Z ");
+   Serial.println("Reciver");
+  }
+  else if(msg[0] == 9)
+  {
+   Serial.print(" Nunchuk Foward");
+   Serial.println(" Reciver");
+  }
+  else if(msg[0] == 10)
+  {
+   Serial.print(" Nunchuk Back");
+   Serial.println(" Reciver");
+  }
+  else if(msg[0] == 11)
+  {
+ Serial.println(" Stop Reciver");
+ digitalWrite(in1, LOW);
+ digitalWrite(in2, LOW);
+ digitalWrite(in3, LOW);
+ digitalWrite(in4, LOW);
+ }
+  }
+  }
